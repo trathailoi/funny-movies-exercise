60af3659f3b3a93409e57cfe069afe87ee194f8b
Trà Thái Lợi <trathailoi@gmail.com>
2022-04-07 15:39:31 +0700

    refactor(pagehome): skeleton loading

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,53 +1,165 @@
 <template>
   <n-list>
-    <n-list-item v-for="item in movies" :key="item.id">
-      <n-grid x-gap="12" :cols="2">
-        <n-gi class="flex justify-end">
-          <n-image class="flex-none" :src="item.thumbnailPath" :alt="item.title" />
-        </n-gi>
-        <n-gi>
-          <n-thing>
-            <template #header>
-              <strong class="text-red">{{ item.title }}</strong>
-            </template>
-            <template #description>
-              Shared by: {{ item.author && item.author.email || 'Unknown' }}
-            </template>
-            {{ item.desc }}
-            <template #action>
-              <n-space>
-                <n-button
+    <template v-if="loading">
+      <n-list-item>
+        <n-grid x-gap="12" :cols="2">
+          <n-gi class="flex justify-end">
+            <n-skeleton
+              :width="350"
+              :height="250"
+              :sharp="false"
+              size="medium"
+            />
+          </n-gi>
+          <n-gi>
+            <n-thing>
+              <template #header>
+                <n-skeleton
+                  :width="200"
+                  :sharp="false"
                   size="medium"
-                  :disabled="!(user && user.email)"
-                  :type="item.likes.includes(user.id || '') ? 'primary' : 'default'"
-                  @click="hitThumb(item, LikeType.Like)"
-                >
-                  <template #icon>
-                    <n-icon>
-                      <i-mdi-thumb-up-outline />
-                    </n-icon>
-                  </template>
-                  {{ item.likes.length }}
-                </n-button>
-                <n-button
+                />
+              </template>
+              <template #description>
+                <n-skeleton
+                  :width="150"
+                  :sharp="false"
                   size="medium"
-                  :disabled="!(user && user.email)"
-                  :type="item.dislikes.includes(user.id || '') ? 'primary' : 'default'"
-                  @click="hitThumb(item, LikeType.Dislike)"
-                >
-                  <template #icon>
-                    <n-icon>
-                      <i-mdi-thumb-down-outline />
-                    </n-icon>
-                  </template>
-                  {{ item.dislikes.length }}
-                </n-button>
-              </n-space>
-            </template>
-          </n-thing>
-        </n-gi>
-      </n-grid>
-    </n-list-item>
+                />
+              </template>
+              <n-skeleton
+                :width="300"
+                :height="100"
+                :sharp="false"
+                size="medium"
+              />
+              <template #action>
+                <n-space>
+                  <n-skeleton
+                    :width="60"
+                    :height="35"
+                    :sharp="false"
+                    :round="true"
+                    size="medium"
+                  />
+                  <n-skeleton
+                    :width="60"
+                    :height="35"
+                    :sharp="false"
+                    :round="true"
+                    size="medium"
+                  />
+                </n-space>
+              </template>
+            </n-thing>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+      <n-list-item>
+        <n-grid x-gap="12" :cols="2">
+          <n-gi class="flex justify-end">
+            <n-skeleton
+              :width="350"
+              :height="250"
+              :sharp="false"
+              size="medium"
+            />
+          </n-gi>
+          <n-gi>
+            <n-thing>
+              <template #header>
+                <n-skeleton
+                  :width="200"
+                  :sharp="false"
+                  size="medium"
+                />
+              </template>
+              <template #description>
+                <n-skeleton
+                  :width="150"
+                  :sharp="false"
+                  size="medium"
+                />
+              </template>
+              <n-skeleton
+                :width="300"
+                :height="100"
+                :sharp="false"
+                size="medium"
+              />
+              <template #action>
+                <n-space>
+                  <n-skeleton
+                    :width="60"
+                    :height="35"
+                    :sharp="false"
+                    :round="true"
+                    size="medium"
+                  />
+                  <n-skeleton
+                    :width="60"
+                    :height="35"
+                    :sharp="false"
+                    :round="true"
+                    size="medium"
+                  />
+                </n-space>
+              </template>
+            </n-thing>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+    </template>
+    <template v-else>
+      <n-list-item v-for="item in movies" :key="item.id">
+        <n-grid x-gap="12" :cols="2">
+          <n-gi class="flex justify-end">
+            <n-image class="flex-none" :src="item.thumbnailPath" :alt="item.title" />
+          </n-gi>
+          <n-gi>
+            <n-thing>
+              <template #header>
+                <strong class="text-red">{{ item.title }}</strong>
+              </template>
+              <template #description>
+                Shared by: {{ item.author && item.author.email || 'Unknown' }}
+              </template>
+              {{ item.desc }}
+              <template #action>
+                <n-space>
+                  <n-button
+                    size="medium"
+                    :disabled="!(user && user.email)"
+                    :type="item.likes.includes(user.id || '') ? 'primary' : 'default'"
+                    @click="hitThumb(item, LikeType.Like)"
+                  >
+                    <template #icon>
+                      <n-icon>
+                        <i-mdi-thumb-up-outline />
+                      </n-icon>
+                    </template>
+                    {{ item.likes.length }}
+                  </n-button>
+                  <n-button
+                    size="medium"
+                    :disabled="!(user && user.email)"
+                    :type="item.dislikes.includes(user.id || '') ? 'primary' : 'default'"
+                    @click="hitThumb(item, LikeType.Dislike)"
+                  >
+                    <template #icon>
+                      <n-icon>
+                        <i-mdi-thumb-down-outline />
+                      </n-icon>
+                    </template>
+                    {{ item.dislikes.length }}
+                  </n-button>
+                </n-space>
+              </template>
+            </n-thing>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+    </template>
   </n-list>
 </template>
 
@@ -82,8 +194,10 @@ const { user } = storeToRefs(useRootStore())
 const message = useMessage()
 
 const movies = ref<movieItem[] | null>([])
+const loading = ref(true)
 
 const fetchMovies = async () => {
+  loading.value = true
   try {
     const { data } = await getMovies()
     movies.value = data.data
@@ -96,6 +210,7 @@ const fetchMovies = async () => {
       message.error(err.message)
     }
   }
+  loading.value = false
 }
 
 const hitThumb = async (mit: movieItem, actionType: LikeType) => {
