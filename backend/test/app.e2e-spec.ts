0f7f3a38ff05c47fd3a1496c1c0533faad85fd58
Trà Thái Lợi <trathailoi@gmail.com>
2022-04-15 13:58:29 +0700

    test(e2e): main user stories (positive cases only)

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -13,6 +13,16 @@ const sampleUserCredentials = {
   email: 'somebody@hotmail.com',
   password: 'Y0uKnowWh@tItIs'
 }
+const sampleMovie = {
+  title: 'Movie Title',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  thumbnailPath: 'https://via.placeholder.com/150x150',
+  srcPath: '/path/to/movie.mp4'
+  // author: '5f36215d-f5c6-4896-087c-f30f3678f607'
+}
+let userId = ''
+let movieId = ''
+let token = ''
 
 describe('Main user stories only (sign up --> sign in --> create movie -> like/dislike/unlike)', () => {
   let app: INestApplication
@@ -52,7 +62,7 @@ describe('Main user stories only (sign up --> sign in --> create movie -> like/d
   })
 
   it('should signup a new user #POSITIVE', async () => {
-    const signUpResponse = await request(httpServer)
+    const response = await request(httpServer)
       .post('/authen/signup')
       .set('Content-type', 'application/json')
       .send({
@@ -60,7 +70,9 @@ describe('Main user stories only (sign up --> sign in --> create movie -> like/d
         confirmPassword: sampleUserCredentials.password
       })
       .expect(201)
-    expect(signUpResponse.status).toEqual(201)
+    expect(response.status).toEqual(201)
+    expect(response.body).toHaveProperty('id')
+    userId = response.body.id
   })
 
   it('should sign that user in #POSITIVE', async () => {
@@ -70,5 +82,87 @@ describe('Main user stories only (sign up --> sign in --> create movie -> like/d
       .send(sampleUserCredentials)
       .expect(200)
     expect(response.status).toEqual(200)
+    token = response.body.access_token
+  })
+
+  it('should create a movie successfully #POSITIVE', async () => {
+    const creatingResponse = await request(httpServer)
+      .post('/movies')
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send(sampleMovie)
+      .expect(201)
+    expect(creatingResponse.status).toEqual(201)
+    expect(creatingResponse.body).toHaveProperty('id')
+    movieId = creatingResponse.body.id
+
+    const gettingResponse = await request(httpServer)
+      .get(`/movies/${movieId}`)
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    expect(gettingResponse.status).toEqual(200)
+    expect(gettingResponse.body).toEqual(expect.objectContaining(sampleMovie))
+    expect(gettingResponse.body.id).toBe(movieId)
+  })
+
+  it('should get the movies list WITH authorization #POSITIVE', async () => {
+    const response = await request(httpServer)
+      .get('/movies')
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    expect(response.status).toEqual(200)
+    expect(response.body.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(sampleMovie)
+      ])
+    )
+  })
+
+  it('should get the movies list WITHOUT authorization #POSITIVE', async () => {
+    const response = await request(httpServer)
+      .get('/movies')
+      .set('Content-type', 'application/json')
+      .expect(200)
+    expect(response.status).toEqual(200)
+    expect(response.body.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(sampleMovie)
+      ])
+    )
+  })
+
+  it('should dislike a movie successfully #POSITIVE', async () => {
+    const creatingResponse = await request(httpServer)
+      .patch(`/movies/${movieId}/reactions?action=dislike`)
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    expect(creatingResponse.status).toEqual(200)
+    expect(creatingResponse.body.message).toBe('success')
+    expect(creatingResponse.body.dislikes).toEqual(expect.arrayContaining([userId]))
+  })
+
+  it('should like a movie successfully #POSITIVE', async () => {
+    const creatingResponse = await request(httpServer)
+      .patch(`/movies/${movieId}/reactions?action=like`)
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    expect(creatingResponse.status).toEqual(200)
+    expect(creatingResponse.body.message).toBe('success')
+    expect(creatingResponse.body.likes).toEqual(expect.arrayContaining([userId]))
+  })
+
+  it('should unlike a movie successfully #POSITIVE', async () => {
+    const creatingResponse = await request(httpServer)
+      .patch(`/movies/${movieId}/reactions?action=like-undo`)
+      .set('Content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+    expect(creatingResponse.status).toEqual(200)
+    expect(creatingResponse.body.message).toBe('success')
+    expect(creatingResponse.body.likes).toEqual(expect.not.arrayContaining([userId]))
   })
 })
